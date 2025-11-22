02e050e9f7b41832f5453f98d45a64358d2c1c3e
Abdulrahman-Mohamad <abd.rahman.mohamad.93@gmail.com>
2025-11-22 23:48:17 +0300

    finish Dock Presentaion

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import Dock from "#components/Dock";
 import Navbar from "#components/Navbar";
 import Welcome from "#components/Welcome";
 
 export default function App() {
     return <>
         <main>
-            <Navbar/>
-            <Welcome/>
+            <Navbar />
+            <Welcome />
+            <Dock />
         </main>
     </>
 }
