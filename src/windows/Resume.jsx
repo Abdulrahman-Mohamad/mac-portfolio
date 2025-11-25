fd228054f7b0c2ff0306b1b03a39946464f23eb8
Abdulrahman-Mohamad <abd.rahman.mohamad.93@gmail.com>
2025-11-25 07:03:50 +0300

    test chang to cdn

--- src/windows/Resume.jsx
+++ src/windows/Resume.jsx
@@ -7,10 +7,8 @@ import { Document, Page, pdfjs } from 'react-pdf';
 import 'react-pdf/dist/Page/AnnotationLayer.css';
 import 'react-pdf/dist/Page/TextLayer.css';
 
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-    'pdfjs-dist/build/pdf.worker.min.mjs',
-    import.meta.url,
-).toString();
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
+
 
 const Resume = () => {
     const fileUrl = useMemo(() => "/files/cv.pdf", []);
